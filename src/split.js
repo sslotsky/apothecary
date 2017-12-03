6dbb3ae2c310c2da7deefe87cd352aca67472218
Sam Slotsky <saxosamo@gmail.com>
2017-12-02 22:22:10 -0600

    Split actions use defaults to allow lazy state tree augmentation

--- src/split.js
+++ src/split.js
@@ -1,15 +1,16 @@
 const drill = (state, action, keys) => {
   const key = keys[0];
+  const substate = state[key];
 
   if (keys.length === 1) {
     const key = keys[0];
 
-    return { ...state, [key]: action(state[key]) };
+    return { ...state, [key]: action(substate) };
   }
 
   return {
     ...state,
-    [key]: drill(state[key], action, keys.slice(1))
+    [key]: drill(substate || {}, action, keys.slice(1))
   };
 };
 
